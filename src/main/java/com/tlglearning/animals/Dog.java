be205285fe02c7102c5719e10b6f018dc9979266
bluedestiny88 <bluedestiny88@gmail.com>
2022-09-20 10:06:45 -0700

    Initialization demonstration

--- src/main/java/com/tlglearning/animals/Dog.java
+++ src/main/java/com/tlglearning/animals/Dog.java
@@ -2,6 +2,36 @@ package com.tlglearning.animals;
 
 public class Dog extends Wolf {
 
+  static  {
+    System.out.println("Dog initialization block");
+  }
+
+  private static int initializeValue()  {
+    System.out.println("Dog.initializeValue");
+    return -2;
+  };
+
+  private static final int MY_CONSTANT = initializeValue();
+
+  private int initializeData()  {
+    System.out.println("Dog.initializeData");
+    return 2;
+  }
+
+  public Dog()  {
+    System.out.println("Dog constructor");
+  }
+
+  {
+    System.out.println("Dog instance initialization block");
+  }
+
+  public int data = initializeData();
+
+  {
+    System.out.println("Second Dog initialization block");
+  }
+
   @Override
   public void vocalize() {
     System.out.println("Bark like a dog!");
--- src/main/java/com/tlglearning/animals/Wolf.java
+++ src/main/java/com/tlglearning/animals/Wolf.java
@@ -2,16 +2,42 @@ package com.tlglearning.animals;
 
 public class Wolf {
 
-  public void vocalize()  {
+  private static final int MY_CONSTANT = initializeValue();
 
-    System.out.println("Howl like a wolf!");
+  static {
+    System.out.println("Wolf static initialization block");
   }
 
-  public void hunt()  {
-    System.out.println("Hunt in packs for live prey");
+  private int data = initializeData();
+
+  {
+    System.out.println("Wolf instance initialization block");
+  }
+
+  public Wolf() {
+    System.out.println("Wolf constructor");
+  }
+
+  private static int initializeValue() {
+    System.out.println("Wolf.initializeValue");
+    return -1;
   }
 
   public static void whoAmI() {
     System.out.println("I am a wolf");
+  }
+
+  private int initializeData() {
+    System.out.println("Wolf.initializeData");
+    return -1;
+  }
+
+  public void vocalize() {
+
+    System.out.println("Howl like a wolf!");
+  }
+
+  public void hunt() {
+    System.out.println("Hunt in packs for live prey");
   }
 }
